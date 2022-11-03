693acbb58c9f2fd98dc91e3755abd65ba3868353
RegularThunder <115854394+RegularThunder@users.noreply.github.com>
2022-11-03 06:18:20 -0700

    Update ScriptLoader.lua

--- ScriptLoader.lua
+++ ScriptLoader.lua
@@ -7,5 +7,5 @@ local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/RegularThunder/Protected/main/BF.lua")()
 else
-    game.Players.LocalPlayer:Kick("Script not Supported!!!")
+    game.Players.LocalPlayer:Kick("Script not Supported!!")
 end
